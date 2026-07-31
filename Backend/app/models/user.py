1cb30d8a804f25187937e6a41e6f36396065f431
itachi1920x1080 <itachi1920x1080@gmail.com>
2026-07-31 15:13:39 +0700

    feat: create User model and automatically generate tables in MySQL

---
+++ Backend/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.db.database import Base
+
+class User (Base):
+   __tablename__ = "users"
+
+   id =  Column(Integer,primary_key= True,index =True )
+   username =Column(String(50),unique= True,index=True,nullable= False )
+   email = Column(String(100),unique = True , index = True , nullable = False)
+   hashed_password = Column(String(255),nullable = False)
+   is_active = Column(Boolean, default=True)
+   role = Column(String(20), default="admin")  #  (admin, cashier, manager)   
+   
+    
--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.db.database import engine, Base, get_db
+from app.models import user
+Base.metadata.create_all (bind= engine)
 app = FastAPI(title="Coffee Shop API")
 
 @app.get("/")
 def read_root():
     return {"message":"Coffee Shop AIP Running "}
-    
+
+
+@app.get("/db_tests")
+def checke_db_connection (db:Session = Depends(get_db)):
+    return { 
+        "status " : "success",
+        "message" : "Database connected successfully and table  are create !"
+    }
